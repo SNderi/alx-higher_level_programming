fce38895362cd087e11deae1d4bb6a820dc63eae
Sharon Nderi <nderisharonw@gmail.com>
2022-05-31 17:58:06 +0000

    Add newline

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100):
     if n == 99:
-        print("{:0=2d}".format(n), end=" ")
+        print("{:0=2d}\n".format(n), end=" ")
         continue
     print("{:0=2d},".format(n), end=" ")
