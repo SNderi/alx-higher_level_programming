4a121a8f0742578b286a8f181039d2f4fbea7a3e
Sharon Nderi <nderisharonw@gmail.com>
2022-06-02 15:36:51 +0000

    Remove break statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    size = len(argv) - 1
+    size = len(sys.argv) - 1
     if size == 1:
         print("{} argument:".format(size))
     elif size == 0:
-        print("{} arguments.".format(size))
-        break
+        print("{} arguments.".format(size)
     else:
         print("{} arguments:".format(size))
     for i in range(0, len(sys.argv)):
