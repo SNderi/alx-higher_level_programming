b6b9968f7ad6291912391cdb17fab7a95556af0a
Sharon Nderi <nderisharonw@gmail.com>
2022-06-02 15:39:55 +0000

    Print formatting edit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ from sys import argv
 if __name__ == "__main__":
     size = len(argv) - 1
     if size == 1:
-        print("{} argument:".format(size))
+        print("{:d} argument:".format(size))
     elif size == 0:
-        print("{} arguments.".format(size)
+        print("{:d} arguments.".format(size)
     else:
-        print("{} arguments:".format(size))
+    print("{:d} arguments:".format(size))
     for i in range(0, len(argv)):
         if i > 0:
-            print("{}: {}".format(i, argv[i]))
+        print("{:d}: {:s}".format(i, argv[i]))
