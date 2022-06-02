ae9a7a5f6b120e86d058a5c42ebbe881c2c9b072
Sharon Nderi <nderisharonw@gmail.com>
2022-06-02 15:43:50 +0000

    Pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if size == 1:
         print("{:d} argument:".format(size))
     elif size == 0:
-        print("{:d} arguments.".format(size)
+        print("{:d} arguments.".format(size))
     else:
         print("{:d} arguments:".format(size))
     for i in range(0, len(argv)):
         if i > 0:
-        print("{:d}: {:s}".format(i, argv[i]))
+            print("{:d}: {:s}".format(i, argv[i]))
