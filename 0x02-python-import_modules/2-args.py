2d55e5d7cc57c984faf4c279a09e8f1d18f0f7f1
Sharon Nderi <nderisharonw@gmail.com>
2022-06-02 15:38:22 +0000

    Correction on import statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 if __name__ == "__main__":
-    size = len(sys.argv) - 1
+    size = len(argv) - 1
     if size == 1:
         print("{} argument:".format(size))
     elif size == 0:
         print("{} arguments.".format(size)
     else:
         print("{} arguments:".format(size))
-    for i in range(0, len(sys.argv)):
+    for i in range(0, len(argv)):
         if i > 0:
-            print("{}: {}".format(i, sys.argv[i]))
+            print("{}: {}".format(i, argv[i]))
