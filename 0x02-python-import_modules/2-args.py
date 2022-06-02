d972f32e989f1cf67fefd85cb5fd7fc769fc23fc
Sharon Nderi <nderisharonw@gmail.com>
2022-06-02 15:41:47 +0000

    Indentation correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     elif size == 0:
         print("{:d} arguments.".format(size)
     else:
-    print("{:d} arguments:".format(size))
+        print("{:d} arguments:".format(size))
     for i in range(0, len(argv)):
         if i > 0:
         print("{:d}: {:s}".format(i, argv[i]))
