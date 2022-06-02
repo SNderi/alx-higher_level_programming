e7c51284666927cfbb53f6bc730f250d15619f56
Sharon Nderi <nderisharonw@gmail.com>
2022-06-02 13:34:10 +0000

    Print argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    size = len(sys.argv) - 1
+    if size == 1:
+        print("{} argument:".format(size))
+    elif size == 0:
+        print("{} arguments.".format(size))
+    else:
+        print("{} arguments:".format(size))
+    for i in range(2, len(sys.argv)):
+        print("{}: argv[i]".format(i - 1))
+
