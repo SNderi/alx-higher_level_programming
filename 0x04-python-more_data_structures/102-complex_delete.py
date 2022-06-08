ea93c445d8cc0e7a823fe9f2f72a017e417b716a
Sharon Nderi <nderisharonw@gmail.com>
2022-06-08 19:54:43 +0000

    Correction on key fetch

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if value in a_dictionary.values():
-        keys = list(a_dictionary.keys())
-        values = list(a_dictionary.values())
-        for val in values:
-            if val == value:
-                i = values.index(val)
-                j = keys[i]
-                a_dictionary.pop(j)
+    for key in list(a_dictionary):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
     return(a_dictionary)
