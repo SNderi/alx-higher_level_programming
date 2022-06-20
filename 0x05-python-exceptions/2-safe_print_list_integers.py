8ebd567ba87bced07c699c38ea8bdab4ac48c71c
Sharon Nderi <nderisharonw@gmail.com>
2022-06-20 20:03:48 +0000

    Include TypeError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             i += 1
         except IndexError:
             raise IndexError
-        except ValueError:
+        except (ValueError, TypeError):
             i += 1
             continue
         else:
