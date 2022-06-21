5a3d4dce262e284e882f374dd956bbf81d738cd4
SNderi <nderisharonw@gmail.com>
2022-06-22 02:55:46 +0300

    revert changes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -75,8 +75,7 @@ class Square:
             return
         for j in range(self.__position[1]):
             print()
-        a = " " * self.__position[0]
         for i in range(self.__size):
-            #for k in range(self.__position[0]):
-                #print(" ", end="")
-            print(a + '#' * self.__size)
+            for k in range(self.__position[0]):
+                print(" ", end="")
+            print('#' * self.__size)
