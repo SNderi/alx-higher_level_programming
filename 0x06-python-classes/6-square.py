c80a0e63273bc83122e1d225dc82c2b5541cebd4
Sharon Nderi <nderisharonw@gmail.com>
2022-06-21 21:32:31 +0000

    Pycodestyle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,7 +54,7 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         '''Initializes data.
         Args:
             size (int): Default length of Square.
