152389cff8c06048e4ea6e0144dbe18018207433
SNderi <nderisharonw@gmail.com>
2022-06-23 00:38:38 +0300

    Correct position validation check

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,11 +46,11 @@ class Square:
                 value (int): Tuple of 2 positive integers.
             Raises:
                 TypeError: If value isn't a tuple of 2 positive integers.'''
-        if not isinstance(value, tuple) and len(value) != 2:
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        if not isinstance(value[0], int) and not isinstance[value[1], int]:
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError('position must be a tuple of 2 positive integers')
-        if value[0] < 0 and value[1] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
