df4cf900f480e14955df0f1303586458c324bd3c
SNderi <nderisharonw@gmail.com>
2022-07-02 00:30:32 +0300

    removed unnecessary check

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -20,8 +20,6 @@ def print_square(size):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
-    if size == 0:
-        print()
-
+    
     for i in range(size):
         print("#" * size)
