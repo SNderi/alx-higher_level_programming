b5a1152e1180c9aadd1d7566f56d77f8359858c6
Sharon Nderi <nderisharonw@gmail.com>
2022-07-05 19:36:07 +0000

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,7 @@
 
 import sys
 import json
+import os.path
 
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
@@ -13,7 +14,9 @@ file = 'add_item.json'
 
 arg_list = []
 
-arg_list = load(file)
+if os.path.exists(file) and os.path.getsize(file) > 0:
+    arg_list = load(file)
+
 if len(sys.argv) > 1:
     for arg in sys.argv[1:]:
         arg_list.apend(arg)
