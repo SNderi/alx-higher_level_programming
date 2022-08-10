da4c7e956d96996f0b71ae0929bd21ef40beecfc
Sharon Nderi <nderisharonw@gmail.com>
2022-08-10 23:54:44 +0000

    Corrections

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- A script that creates the MySQL server user user_0d_1
 
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grants all privileges on your MySQL server to user_0d_1
 GRANT ALL PRIVILEGES
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS  hbtn_0d_2;
 
 -- creates user user_0d_2
-CREATE USER IF NOT EXISTS  user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
+CREATE USER IF NOT EXISTS  user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grants SELECT privilege to user_0d_2 on hbtn_0d_2
 GRANT SELECT
