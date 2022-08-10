df9ad604a3d68c8f5b773e9f579843beb07cf3b6
Sharon Nderi <nderisharonw@gmail.com>
2022-08-10 23:37:43 +0000

    More queries tasks

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- A script that creates the MySQL server user user_0d_1
+
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+
+-- Grants all privileges on your MySQL server to user_0d_1
+GRANT ALL
+ON *.*
+TO user_0d_1@localhost;
