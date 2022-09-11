cdbf76a3bda30f3456dad4a0f67a068add0ced15
Sharon Nderi <nderisharonw@gmail.com>
2022-09-11 20:09:52 +0000

    Refine filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
                            passwd=args[2], db=args[3])
     cur = conn.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name LIKE '{}'\
+    cur.execute("SELECT id, name FROM states WHERE name LIKE BINARY '{}'\
                 ORDER BY id".format(args[4]))
     rows = cur.fetchall()
     for row in rows:
