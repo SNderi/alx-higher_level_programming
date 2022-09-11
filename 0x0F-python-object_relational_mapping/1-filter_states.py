eb2deff8a237943fee0915e4b09d068f8242a1d1
Sharon Nderi <nderisharonw@gmail.com>
2022-09-11 20:08:53 +0000

    Refine filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     cur = conn.cursor()
     cur.execute("""SELECT id, name FROM states WHERE name
-                LIKE 'N%' ORDER BY id""")
+                LIKE BINARY 'N%' ORDER BY id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
