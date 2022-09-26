22c48f1c3553e0df013081f113959b39fb2851be
SNderi <nderisharonw@gmail.com>
2022-09-26 23:37:49 +0300

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,9 @@ value of the variable X-Request-Id in the response header.
 if __name__ == "__main__":
     import requests
     import sys
-    r = requests.get(sys.argv[1])
-    print(r.headers['X-Request-Id'])
+    
+    try:
+        r = requests.get(sys.argv[1])
+        print(r.headers['X-Request-Id'])
+    except Exception:
+        pass
